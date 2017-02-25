4000c863b7a5f5545409a6d2c1459a8871587a8b
patelmanish1994 <manishkumarpatel1994@gmail.com>
2017-02-25 10:51:00 +0530

    speaking program
    speaking program via python

---
+++ speak.py
@@ -0,0 +1,14 @@
+
+
+# this python program speaks
+#no need of the internet
+#voice is of the female
+
+#this program is set to run when the OS Start
+#the program should be in home directory as it is automatically mounted
+# this file is added in startup application as:python speak.py 
+
+#this  python program run a mp3 file
+import webbrowser
+webbrowser.open("/home/manishkumar/pythonprogram/good3.mp3")
+
